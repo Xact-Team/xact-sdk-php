9c181505745f1d2e4b4279f78aa6e62a400b6f64
Wincey <vincent@sylleb.io>
2021-08-13 16:18:45 +0200

    feat: update createNFTDto

--- Models/CreateNFTDto.php
+++ Models/CreateNFTDto.php
@@ -4,24 +4,26 @@ namespace Xact\Models;
 
 class CreateNFTDto
 {
-    /* Name of the Token */
-    public $name; /* string */
+    public $nft: {
+      /* Name of the Token */
+        public $name; /* string */
 
-    /* Description of the Token */
-    public $description; /* string */
+        /* Description of the Token */
+        public $description; /* string */
 
-    /* Category of the Token */
-    public $category; /* string */
+        /* Category of the Token */
+        public $category; /* string */
 
-    /* Creator of the Token */
-    public $creator; /* string */
+        /* Creator of the Token */
+        public $creator; /* string */
 
-    /* Media to linked to the NFT - Must be in base64 */
-    public $media; /* string:base64 */
-
-    /* NFT's Quantity */
-    public $supply; /* string */
+        /* NFT's Quantity */
+        public $supply; /* string */
+    },
 
     /* Which Account ID issue the NFT's Creation */
     public $fromAccountId; /* string */
+
+    /* Media linked to the NFT - Must be in base64 */
+    public $media; /* string:base64 */
 }
