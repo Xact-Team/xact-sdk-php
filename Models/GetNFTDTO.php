c751b09c88faf9698de888e511afc4a88e3ee263
Wincey <vincent.sder@gmail.com>
2021-12-04 15:30:40 -0400

    feat: adding getNFTDTO

---
+++ Models/GetNFTDTO.php
@@ -0,0 +1,12 @@
+<?php
+namespace Xact\Models;
+
+class GetNFTDTO
+{
+    /* Token ID of the NFT */
+    public $tokenId;
+    /* nftId */
+    public $nftId;
+   /* Custom Uniq Id - Identify Browser's that made the request  */
+    public $uniqId; /* string */
+}
